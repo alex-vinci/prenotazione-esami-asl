7615494827d843dc2bd564b353971878af6d8e29
Sandro <alex.vinci07@gmail.com>
2023-05-21 14:27:18 +0200

    init

---
+++ src/main/java/com/premotazione/esami/asl/PrenotazioneEsamiAslApplication.java
@@ -0,0 +1,13 @@
+package com.premotazione.esami.asl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenotazioneEsamiAslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrenotazioneEsamiAslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/premotazione/esami/asl/PrenotazioneEsamiAslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premotazione.esami.asl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrenotazioneEsamiAslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
